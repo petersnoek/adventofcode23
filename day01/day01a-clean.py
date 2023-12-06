e44dd53a52c872a09e8776418a738452b0764eb9
petersnoek <psnoek@davinci.nl>
2023-12-06 10:56:32 +0100

    day 1, part 2

--- day01/day01a-clean.py
+++ day01/day01a-clean.py
@@ -10,17 +10,28 @@ input_file = os.path.join(sys.path[0], "input01.txt")
 with open(input_file) as f:
     lines = f.read().splitlines()
 
-line_counter = 1
+replace = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+    'zero': '0',
+}
+
 first_digit = 0
 last_digit = 0
 total = 0
 for line in lines:
+    replaced = line.translate(replace)
     f = re.search(r'\d', line).group(0)
     l = re.search(r'(\d)(?!.*\d)', line).group(0)
     number = int(f + l)
     total = total + number
-    print("Line: ", line_counter, ": ", line, f, l, number, total)
-    line_counter = line_counter + 1
 
 print('---------------')
 print('ANSWER: ', total)
--- day01/day01b.py
+++ day01/day01b.py
@@ -2,9 +2,60 @@
 
 import os
 import sys
+import re
 
 # read input file into "lines"
-input_file = os.path.join(sys.path[0], "example01.txt")
-# input_file = os.path.join(sys.path[0], "input01.txt")
+# input_file = os.path.join(sys.path[0], "example01b.txt")
+input_file = os.path.join(sys.path[0], "input01.txt")
 with open(input_file) as f:
     lines = f.read().splitlines()
+
+valid_items = [
+    'one', '1',
+    'two', '2',
+    'three', '3',
+    'four', '4',
+    'five', '5',
+    'six', '6',
+    'seven', '7',
+    'eight', '8',
+    'nine', '9',
+    'zero', '0',
+]
+
+replace_word_to_number = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+    'zero': '0',
+}
+
+total = 0
+for line in lines:      # eightwothree
+    matches = []
+    for x in range(len(line)):    # loop over alle karakters in de regel: 0, 1, 2, ... 11
+        part = line[x:]    # neem steeds een nieuw stukje tekst, 0:eightwothree,  1:ightwothree
+
+        for item in valid_items:    # one, 1, two, 2
+            if part.startswith(item):
+                if item in replace_word_to_number:  # moet 't vervangen worden?
+                    matches.append(replace_word_to_number[item])
+                else:       # het is al een getal (0, 1, 2)
+                    matches.append(item)
+        # print(line, part, matches)
+
+    first_digit = matches[0]
+    last_digit = matches[-1]
+
+    total = total + int(first_digit + last_digit)
+
+    print(line, first_digit, last_digit, total)
+
+print('---------------')
+print('ANSWER: ', total)
